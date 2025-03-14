3b97f4abf8a7bf9142e807aa697027deed6a188e
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-14 11:07:20 +0100

    feat: add request timestamp middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// middleware
+app.use((req, res, next) => {
+  const now = Date.now();
+  req.requestTime = now;
+  next();
+});
+
+// data sent to the user
+app.get("/", (req, res) => {
+  res.send(req.requestTime.toString());
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
