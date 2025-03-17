c73562eb07667eca08161924a146db74e2e8cfae
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-17 14:45:17 +0100

    refactor: remove rate limiting and dotenv dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
-
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const userRoutes = require("./routes/users")
 const { hashPassword } = require("./middleware/password-encrypt")
-const limiter = require('./middleware/rateLimiter');
 const requestLogger = require('./middleware/logger');
-
 
 // MIDDLEWARE
 app.use(express.json());
@@ -20,21 +16,17 @@ app.use((req, res, next) => {
    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   )
   next()
- })
+});
 
 app.use(express.static('src/public'));
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API ! e-commerce backed 🤳")
- })
+});
+
+app.use("/api/users", userRoutes);
+app.use(requestLogger);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-app.use("/api/users", userRoutes);
-
-// Apply rate limiting
-app.use('/api', limiter);
-
-app.use(requestLogger);
