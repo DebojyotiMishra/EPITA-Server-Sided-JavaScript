554fb3dff38d1f93ae00d8d37ded470d9e550360
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-03-25 10:48:05 +0100

    fix: add mongoose unique validator

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -40,6 +41,6 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-userSchema.plugin(uniqueValidator)
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema); 
